82279bcaee015075a26f3a86906b7a4caa252f05
Ping <pegasusplus@hotmail.com>
2020-06-23 16:20:56 +0800

    Simplify Label ref cycle detection

--- DNSGateway/DNSPacket.Helper.cs
+++ DNSGateway/DNSPacket.Helper.cs
@@ -16,7 +16,8 @@ namespace Kaitai
             public string GetFullName()
             {
                 StringBuilder sb = new StringBuilder();
-                DoGetFullName(sb, null);
+                //DoGetFullName(sb, null);
+                DoGetFullName(sb, 0);
                 return sb.ToString();
             }
 
@@ -30,11 +31,14 @@ namespace Kaitai
                         // nPos must be large than 0x09, less than size of the root data
                         if (nPos >= 0x0C && nPos < m_root.m_io.Size)
                         {
+                            // if pointer to pos is after current, it might be a cycle
+                            // how to know current label start pos?
                             if (null == pl)
                             {
                                 pl = new List<ushort>();
                             }
                             // nPos can not be the same of previous one, to avoid cycle
+                            // nPos must be less than previous one - 2
                             bool bHasCycle = false;
                             foreach(ushort p in pl)
                             {
@@ -48,6 +52,7 @@ namespace Kaitai
                             {
                                 pl.Add(nPos);
                                 label.Pointer.Contents.DoGetFullName(sb, pl);
+                                break; // Do not support pointer after pointer. Only last label can be pointer
                             }
                             else
                             {
@@ -57,6 +62,38 @@ namespace Kaitai
                         else
                         {
                             throw new ArgumentOutOfRangeException("requested position " + nPos + " is out of range");
+                        }
+                    }
+                    else
+                    {
+                        if (sb.Length > 0)
+                        {
+                            sb.Append('.');
+                        }
+
+                        sb.Append(label.Name);
+                    }
+                }
+
+                return;
+            }
+            protected void DoGetFullName(StringBuilder sb, ushort nPreviousPos)
+            {
+                foreach (Label label in Name)
+                {
+                    if (label.IsPointer)
+                    {
+                        ushort nPos = (ushort)(label.Pointer.Value + ((label.Length - 192) << 8));
+                        // nPos must be large than 0x09, less than size of the root data
+                        if (nPos >= 0x0C && nPos < m_root.m_io.Size && (0 == nPreviousPos || nPos < nPreviousPos - 1))
+                        {
+                            // nPos must be less than previous pos - 1
+                            label.Pointer.Contents.DoGetFullName(sb, nPos);
+                            break; // Do not support pointer after pointer. Only last label can be pointer
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException("requested position " + nPos + " is out of range or cause cycle");
                         }
                     }
                     else
